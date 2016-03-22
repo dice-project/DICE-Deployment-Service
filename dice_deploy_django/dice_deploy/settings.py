3cc8d3f14792b5272241b6bda4a1a3870b87d859
Miha Pleško <miha.plesko@xlab.si>
2016-03-22 15:10:04 +0100

    Bugfix - enable file upload
    Fixed bug that shipped with djangorestframework
    upgrading.
    
    Refers:
    * https://trello.com/c/JFqIrx5Z/47-dice-deploy-nalaganje-blueprinta-ne-deluje

--- dice_deploy_django/dice_deploy/settings.py
+++ dice_deploy_django/dice_deploy/settings.py
@@ -47,6 +47,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PARSER_CLASSES": (
         "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.MultiPartParser",
+        "rest_framework.parsers.FileUploadParser",
     ),
     "DATETIME_FORMAT": API_DT_FORMAT
 }
