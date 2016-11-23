e49c5404f128be7d660f666786328d3d746f3678
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-11-23 10:55:41 +0100

    Split requirements into regular and development
    It makes no sense to have development libraries such as various mock
    helpers installed in production setting. This commit moved such
    requirements into separate requirements-dev.txt file.
    
    Change-Id: I5bea1cc8ea71b3aa611ba4e87c6586a68f9219ad

--- provision-user.sh
+++ provision-user.sh
@@ -9,7 +9,7 @@ pip install -U pip
 echo '. venv/bin/activate' >> .profile
 
 cd dice_deploy_django
-pip install -r requirements.txt
+pip install -r requirements-dev.txt
 bower install
 
 ./run.sh reset
