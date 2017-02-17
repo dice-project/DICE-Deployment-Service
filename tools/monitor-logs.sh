01e5cff9cc69cf8adeba3ed3da7d4392715ab6ba
Matej Artač <matej.artac@xlab.si>
2017-02-17 12:52:36 +0100

    Add a tool for monitoring and saving logs
    I created a simple tool, which runs forever, polls cfy manager for
    executions, and tails any newly encountered executions with status
    `started`.
    
    Change-Id: I47d744d2dd3980e25abee3dfa1d9edbd6f9531a7

---
+++ tools/monitor-logs.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Iterate forever and slurp any new execution logs
+# on the hard drive
+
+function usage() {
+    echo "Usage: $0 [ log_path | -h ]"
+    echo ""
+    echo "Runs in a loop and tails any execution logs that it "
+    echo "encounters in status started, storing them into a file."
+    echo "The logs are in a DEPLOYMENT_ID/EXECUTION_ID-WORKFLOW_ID.log file."
+    echo ""
+    echo "    log_path: path to where the tool creates the logs "
+    echo "              (default: /tmp/cfy)"
+    echo "    -h: print this usage"
+    echo ""
+}
+
+function ensure_log_path() {
+    mkdir -p "$1"
+}
+
+function run() {
+    LOG_PATH="$1"
+
+    while [ 1 ]
+    do
+        local EXECUTIONS=$(cfy executions list | \
+            grep started | \
+            awk '{print $6 "+" $2 "+" $4}')
+
+        for EXECUTION in $EXECUTIONS
+        do
+            TOKENS=$(echo "$EXECUTION" | \
+                sed 's/+/ /g')
+            EVENT_ID=$(echo "$TOKENS" | \
+                awk '{print $2}')
+            LOG=$(echo "$TOKENS" | \
+                awk '{print $1 "/" $3 "-" $2 ".log"}')
+            LOG="$LOG_PATH/$LOG"
+
+            mkdir -p $(dirname $LOG)
+
+            if [ ! -e "$LOG" ]
+            then
+                echo "starting $LOG"
+
+                touch "$LOG"
+                cfy events list -l --tail -e $EVENT_ID > "$LOG" &
+            fi
+        done
+        sleep 1
+    done
+}
+
+if [ "$1" == "-h" ]
+then
+    usage
+    exit 0
+fi
+
+LOG_PATH="${1:-/tmp/cfy}"
+
+ensure_log_path "$LOG_PATH"
+run "$LOG_PATH"
