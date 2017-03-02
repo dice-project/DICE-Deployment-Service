54b16d3a25fea766b65125c57a7158cedb2417c6
Tadej Borovšak <tadej.borovsak@xlab.si>
2017-03-02 08:38:47 +0100

    Add script for generating TOC in markdown docs
    This script is mainly meant to be executed from vim at the top of the
    markdown document. Exact command that can be used to generate TOC and
    place it on the line below the cursor:
    
        :read !./tocgen.sh %
    
    Change-Id: I427d11740da3fe6e616b25d690afa5af40419edd

---
+++ doc/tocgen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function filter-lines()
+{
+  grep -E "^#{2,3}" "$1" \
+    | sed -r -e 's/^## +(.+[^ ]) *$/ 0. [\1]/' \
+             -e 's/^### +(.+[^ ]) *$/   0. [\1]/'
+}
+
+function produce-link()
+{
+  # Next $1 is naked on purpose to get rid of multiple spaces.
+  echo ${1,,} | grep -Eo "\[.+\]" | sed -e 's/\[/(#/' -e 's/\]/)/' -e 's/ /-/g'
+}
+
+function main() {
+  local line IFS=$'\n'
+
+  for line in $(filter-lines "$1")
+  do
+    echo "$line"$(produce-link "$line")
+  done
+}
+
+if [[ "x" == "x$1" ]]
+then
+  echo "Usage: $0 file.md"
+  exit 1
+else
+  main "$1"
+fi
